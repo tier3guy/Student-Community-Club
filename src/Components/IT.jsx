cb61e8155cc783c262259240e94747c7e4e27c16
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-01 23:31:50 +0530

    restoring gh

--- src/Components/IT.jsx
+++ src/Components/IT.jsx
@@ -3,11 +3,6 @@ export default function IT(){
     <>
       <h1 style = {{textAlign: "center"}}>Information Technology</h1>
       <h2 className = "syllabus_link"> <a rel = "noreferrer" href = "https://www.nit.ac.in/pdf/it-ug-18.pdf" target = "_blank"> SYLLABUS </a> </h2>
-
-      <li><a rel = "noreferrer" href = "https://docs.google.com/document/d/1gcxwpSP-1dkTBT0x9jk3lI4u-wuFuX-j/edit" target = "_blank">Chemistry Viva Question</a></li>
-      <div>
-      
-      </div>
     </>
   );
 }
