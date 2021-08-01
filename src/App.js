58f30c3c6d31e3d30d72219e80f8b1c8505d4a08
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-01 23:37:14 +0530

    homepage path bug fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
    <NavBar/>
    <div id="notice"><a rel = "noreferrer" href= "https://docs.google.com/document/d/18HKtYRtHYU5YO_tLJ8ShqGrKpgi9zBjLEgImSEDcFuc/edit" target="_blank">NOTICE</a></div>
     <Switch>
-      <Route exact path = '/Students-Community-Club' component = {Home}/>
+      <Route exact path = '/' component = {Home}/>
       <Route exact path = '/contributers' component = {Contributers}/>
       <Route exact path = '/resources' component = {Resources}/>
     </Switch>
