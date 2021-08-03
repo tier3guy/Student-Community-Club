4bf613e84598ce2dabcfac4d9144c72dec689b39
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-03 21:02:34 +0530

    all links updated as per my side

--- src/Components/SubjectsWise.js
+++ src/Components/SubjectsWise.js
@@ -2,19 +2,26 @@ const questions = [
   {
     id : 1,
     subject : 'CS',
-    content : [{
-      heading: "Fundamentals of Computer",
-      link: "https://drive.google.com/file/d/1OfzkjDS7MbcYEXt76i9xJ5Pn_mW__mcM/view?usp=sharing"
-    },
-    {
-      heading:"Sample Question Paper 2017",
-      link: "https://drive.google.com/file/d/17gRvm0u8H85OVNSlwBLO3j06GkCb0djG/view?usp=sharing"
-    }]
+    content : [
+      {
+        heading: "Fundamentals of Computer",
+        link: "https://drive.google.com/file/d/1OfzkjDS7MbcYEXt76i9xJ5Pn_mW__mcM/view?usp=sharing"
+      },
+      {
+        heading:"Sample Question Paper 2017",
+        link: "https://drive.google.com/file/d/17gRvm0u8H85OVNSlwBLO3j06GkCb0djG/view?usp=sharing"
+      }
+    ]
   },
   {
     id : 2,
     subject : 'Mathematic',
-    content : []
+    content : [
+      {
+        heading:"Double Integration",
+        link: "https://drive.google.com/file/d/1HjRdeSxw5plDW67eNH7hRCwbp1Fm0AHA/view?usp=sharing"
+      }
+    ]
   },
   {
     id : 3,
@@ -24,7 +31,20 @@ const questions = [
   {
     id : 4,
     subject : 'Chemistry',
-    content : []
+    content : [
+      {
+        heading:"Themodynamics and Thermochemistry",
+        link: "https://drive.google.com/file/d/1jgjS3hRGbTvFLLd6DOvz5HNDzA58v0pd/view?usp=sharing"
+      },
+      {
+        heading: "Electrochemistry",
+        link: "https://drive.google.com/file/d/1tWEQhKKTYVYX33Ti7tBywDGLgfpzodZ1/view?usp=sharing"
+      },
+      {
+        heading: "Chemistry Viva Questions (Expected)",
+        link: "https://drive.google.com/file/d/1gcxwpSP-1dkTBT0x9jk3lI4u-wuFuX-j/view?usp=sharing"
+      }
+    ]
   },
   {
     id : 5,
@@ -34,7 +54,28 @@ const questions = [
   {
     id : 6,
     subject : 'Electrical Engineering',
-    content : []
+    content : [
+      {
+        heading:"AC Fundamentals",
+        link: "https://drive.google.com/file/d/1WChFpMi2WhI2lI_rKvtSRTQlW4KSSD5U/view?usp=sharing",
+      },
+      {
+        heading: "Single Phase Transformer",
+        link: "https://drive.google.com/file/d/1Y9qHt-XVBilLeaxnAywU_J_3HZobSixG/view?usp=sharing"
+      },
+      {
+        heading: "DC Machine",
+        link: "https://drive.google.com/file/d/1GDF4-dISEbb3z-rp4Mj11t7Yq0p5tFGf/view?usp=sharing"
+      },
+      {
+        heading: "Network Theorem",
+        link: "https://drive.google.com/file/d/18UFyKwmEcNpylvOU-ZeaCAiCZEi_stmJ/view?usp=sharing"
+      },
+      {
+        heading: "Star-Delta Connection",
+        link: "https://drive.google.com/file/d/1KEl1QIeFSm6k43phCKBFGC2NPuN9nSlG/view?usp=sharing"
+      }
+    ]
   },
   {
     id : 7,
@@ -49,12 +90,66 @@ const questions = [
   {
     id : 9,
     subject : 'English',
-    content : []
+    content : [
+      {
+        heading: "Sample Question Paper 2018",
+        link: "https://drive.google.com/file/d/1tw1ZLsO3JNsefulEEI8SbQfCtYgP_6F2/view?usp=sharing"
+      },
+      {
+        heading: "Sample Question Paper 2019",
+        link: "https://drive.google.com/file/d/1W98lUf5i2T3O83gImGcJZuuo2Gdmh6Gp/view?usp=sharing"
+      },
+      {
+        heading: "Format of Email Writing",
+        link: "https://drive.google.com/file/d/1Q7F6ORcE6GhdENLbtEgvHl8UYmEdT82T/view?usp=sharing"
+      },
+      {
+        heading: "Format of Resume Writing",
+        link: "https://drive.google.com/file/d/1tyd9cbdAkrXqzBtYBXdj31U00w9fg7bL/view?usp=sharing"
+      },
+      {
+        heading: "Format of Notice Writing",
+        link: "https://drive.google.com/file/d/1-kJLIJurN8ASLlT8GmSmv1xcGNoCGg6h/view?usp=sharing"
+      },
+      {
+        heading: "Format of Order Letter Writing",
+        link: "https://drive.google.com/file/d/1A03oI_ttF7EPIqvI7IPlWoXkl2QodNFZ/view?usp=sharing"
+      },
+      {
+        heading: "Technical Report Writing",
+        link: "https://drive.google.com/file/d/12suFK5y2V8RpX7Dfb1vnE8YdROinroQh/view?usp=sharing"
+      },
+      {
+        heading: "Format of Job Application Writing",
+        link: "https://drive.google.com/file/d/1XXSJlbY11JDvKyj4SpIqRnPjHKro0Jo6/view?usp=sharing"
+      },
+      {
+        heading: "The Night Train at Deoli",
+        link: "https://drive.google.com/file/d/1enIHgB_IK5PQF4HhL2PUgth5nsFbeEDU/view?usp=sharing"
+      },
+      {
+        heading: "Lamb to the Slaughter",
+        link: "https://drive.google.com/file/d/1caMxPc4nvlcJ1Bt1Zx1oMzBHPnC13i6x/view?usp=sharing"
+      },
+    ]
   },
   {
     id : 10,
     subject : 'Graphics and Engineering Drawing',
-    content : []
+    content : [
+      {
+        heading: "Plain Scale",
+        link: "https://drive.google.com/file/d/1KEl1QIeFSm6k43phCKBFGC2NPuN9nSlG/view?usp=sharing"
+      },
+      {
+        heading: "Letter Writing",
+        link: "https://drive.google.com/file/d/1s3P5iqJJdz7q8bCQFdpqaFJLolaaqlVo/view?usp=sharing"
+      },
+      {
+        heading: "Ellipse and Hyperbola",
+        link: "https://drive.google.com/file/d/1RRp_AuAGxzSW6vnGqRbp1n3iCyJaK_uW/view?usp=sharing"
+      }
+    ]
   },
 ];
 
