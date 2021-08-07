f548ff263cfbded1f29193ec8e5fc08b8f5368f5
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-07 19:39:09 +0530

    all size phone responsive done

--- src/Components/Connect.js
+++ src/Components/Connect.js
@@ -54,7 +54,7 @@ export default function Connect(){
            onChange = {getName}
            value = {name}></input>
     <br/>
-    <button type = 'submit' className = 'btn_primary' style = {{marginTop: '1em'}}>Submit</button>
+    <button type = 'submit' className = 'btn_primary submitbtn' style = {{marginTop: '1em'}}>Submit</button>
     <h3 style = {{marginTop: '2em' , lineHeight:'1.7em'}}>Well let me tell you how does this CONTACT ME section works, Once you submitted your mail, I will contact you throught that mail ID within 24 hours and after that you have to mail me the pdf of your study material that you want to publish along with your branch and college, so that I can publish your profile in our contributers section.</h3>
     </div>
     </form>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact(){
     <>
     <div className = 'Contact'>
       <h1 style = {{fontSize:'3em'}}>Share Material</h1>
-      <h3 style = {{marginTop: '0.6em'}}>If you have any useful study material that you like to share, you can connect me through your mail ID.</h3>
+      <h3>If you have any useful study material that you like to share, you can connect me through your mail ID.</h3>
       <Connect/>
     </div>
     </>
