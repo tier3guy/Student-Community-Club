e02d3cb45b3697c927f80f94ca50acbf06fbe552
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-07 20:36:47 +0530

    hamburger funtionality added in home

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import Menu from './Menu';
 
 export default function NavBar(){
+
+  const [ham,setHam] = useState(false);
+  const [display,setDisplay] = useState("none");
+  const toggler = () => {
+    setHam(!ham);
+    if(ham){
+      setDisplay("block");
+    }
+    else{
+      setDisplay("none");
+    }
+  }
+
+
 
   return(
     <>
     <div className = 'navbar'>
       <h3 style= {{fontWeight:'bolder'}}>Student's Community Club</h3>
       <Menu/>
-      <i className="fas fa-bars hamburger"></i>
+      <button 
+        style = {{background: "transparent" , border: "none"}}
+        onClick = {toggler}>
+        <i className="fas fa-bars hamburger"></i>
+      </button>
     </div>
+    <div className = "hamMenu" style = {{display:display}}><Menu/></div>
     </>
   );
 }
--- src/Components/Nav_secondary.js
+++ src/Components/Nav_secondary.js
@@ -1,6 +1,20 @@
 import MenuSeconadary from './Menu_seconadary';
+import { useState } from 'react';
 
 export default function NavSecondary(){
+
+  const [ham,setHam] = useState(false);
+  const [display,setDisplay] = useState("none");
+  const toggler = () => {
+    setHam(!ham);
+    if(ham){
+      setDisplay("block");
+    }
+    else{
+      setDisplay("none");
+    }
+  }
+
 
   return(
     <>
@@ -8,6 +22,12 @@ export default function NavSecondary(){
       <h3 style= {{fontWeight:'bolder'}}>Student's Community Club</h3>
       <MenuSeconadary/>
     </div>
+    <button 
+        style = {{background: "transparent" , border: "none"}}
+        onClick = {toggler}>
+        <i className="fas fa-bars hamburger"></i>
+      </button>
+    <div className = "hamMenu" style = {{display:display}}><MenuSeconadary/></div>
     </>
   );
 }
