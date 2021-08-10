4b077e05fc6cd72fa7605d08fbadd7e040c37e1a
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-08-10 14:16:16 +0530

    final link updation

--- src/Components/Connect.js
+++ src/Components/Connect.js
@@ -4,7 +4,7 @@ export default function Connect(){
   return(
     <>
     <br/>
-    <button type = 'submit' className = 'btn_primary submitbtn' style = {{marginTop: '1em'}}><a href = "https://docs.google.com/forms/d/1BRZOlUZoen6yPekMth-T1Hca0zQh2b7txrpy30r2Xvk/" style = {{color: 'white'}}>Let's Contribute</a></button>
+    <button type = 'submit' className = 'btn_primary submitbtn' style = {{marginTop: '1em'}}><a href = "https://docs.google.com/forms/d/e/1FAIpQLSeStgzeP_jtHoVabx2ioRvvfZCsfd4OrwZ34L5kqkolD-PDIA/viewform?usp=sf_link" style = {{color: 'white'}}>Let's Contribute</a></button>
     <h3 style = {{marginTop: '2em' , lineHeight:'1.7em'}}>Well let me tell you how does this CONTACT ME section works, Once you filled the contribute form, I verify the documents if they are not offensive, I will publish your profile in our contributers section and study material will be published in our Resources Section. The process may take arround a whole day. So keep patience. Thank You ..</h3>
     </>
   );
